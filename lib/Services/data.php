ff64f86a21b2054277f18bd9a794f71e450375df
Bolaji <bolaji.smith@webgears.de>
2017-01-14 13:08:20 +0100

    data dictionary

---
+++ lib/Services/data.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace Reachbird\Services;
+
+
+class data
+{
+
+    public static function getDictionary () {
+
+        $dictionary = [
+           'topic11' => [
+                "topic_label" => 'topic11',
+                'topic_name' => 'Beautiful Girl',
+                'total_likes' => '8066651',
+                'mean_likes' => '1503.289415',
+                'total_comments' => '154284',
+                'mean_comments' => '28.75214312',
+                'engagement' => '0.055071904',
+            ],
+
+            'topic1' => [
+                "topic_label" => 'topic1',
+                'topic_name' => 'Holiday: Landscapes',
+                'total_likes' => '7673323',
+                'mean_likes' => '1489.965631',
+                'total_comments' => '148805',
+                'mean_comments' => '28.89417476',
+                'engagement' => '0.06704275',
+            ],
+
+            'topic16' => [
+                "topic_label" => 'topic16',
+                'topic_name' => 'Enjoyment',
+                'total_likes' => '6288418',
+                'mean_likes' => '1451.620037',
+                'total_comments' => '133273',
+                'mean_comments' => '30.76477378',
+                'engagement' => '0.053668844',
+            ],
+
+            'topic19' => [
+                "topic_label" => 'topic19',
+                'topic_name' => 'Summer',
+                'total_likes' => '5087272',
+                'mean_likes' => '1770.101601',
+                'total_comments' => '80405',
+                'mean_comments' => '27.97668754',
+                'engagement' => '0.054438656',
+            ],
+
+            'topic4' => [
+                "topic_label" => 'topic4',
+                'topic_name' => 'Music',
+                'total_likes' => '4347799',
+                'mean_likes' => '1481.362521',
+                'total_comments' => '64183',
+                'mean_comments' => '21.8681431',
+                'engagement' => '0.055064007',
+            ],
+
+            'topic12' => [
+                "topic_label" => 'topic12',
+                'topic_name' => 'Architecture',
+                'total_likes' => '3844796',
+                'mean_likes' => '1414.047812',
+                'total_comments' => '71546',
+                'mean_comments' => '26.3133505',
+                'engagement' => '0.072631766',
+            ],
+
+            'topic10' => [
+                "topic_label" => 'topic10',
+                'topic_name' => 'Indoors',
+                'total_likes' => '3189226',
+                'mean_likes' => '2030.061108',
+                'total_comments' => '55386',
+                'mean_comments' => '35.25525143',
+                'engagement' => '0.054187257',
+            ],
+
+            'topic18' => [
+                "topic_label" => 'topic18',
+                'topic_name' => 'Winter',
+                'total_likes' => '3156414',
+                'mean_likes' => '1490.280453',
+                'total_comments' => '62941',
+                'mean_comments' => '29.71718602',
+                'engagement' => '0.074744571',
+            ],
+
+            'topic25' => [
+                "topic_label" => 'topic25',
+                'topic_name' => 'Cars',
+                'total_likes' => '3018263',
+                'mean_likes' => '1745.669751',
+                'total_comments' => '28345',
+                'mean_comments' => '16.39386929',
+                'engagement' => '0.044800421',
+            ],
+
+            'topic8' => [
+                "topic_label" => 'topic8',
+                'topic_name' => 'Fashion',
+                'total_likes' => '2912677',
+                'mean_likes' => '1445.49727',
+                'total_comments' => '54594',
+                'mean_comments' => '27.09379653',
+                'engagement' => '0.053508188',
+            ],
+
+            'topic3' => [
+                "topic_label" => 'topic3',
+                'topic_name' => 'Urban',
+                'total_likes' => '2824131',
+                'mean_likes' => '1427.770981',
+                'total_comments' => '55488',
+                'mean_comments' => '28.05257836',
+                'engagement' => '0.060861802',
+            ],
+
+            'topic24' => [
+                "topic_label" => 'topic24',
+                'topic_name' => 'Home',
+                'total_likes' => '2814779',
+                'mean_likes' => '1575.142138',
+                'total_comments' => '40488',
+                'mean_comments' => '22.65696698',
+                'engagement' => '0.053429722',
+            ],
+
+            'topic22' => [
+                "topic_label" => 'topic22',
+                'topic_name' => 'Art/Drawing',
+                'total_likes' => '2749526',
+                'mean_likes' => '1211.244934',
+                'total_comments' => '51785',
+                'mean_comments' => '22.81277533',
+                'engagement' => '0.046811672',
+            ],
+
+            'topic17'=> [
+                "topic_label" => 'topic17',
+                'topic_name' => 'Office',
+                'total_likes' => '2727712',
+                'mean_likes' => '1076.445146',
+                'total_comments' => '76600',
+                'mean_comments' => '30.22888713',
+                'engagement' => '0.043465514',
+            ],
+
+            'topic7' => [
+                "topic_label" => 'topic7',
+                'topic_name' => 'Pets/Dogs',
+                'total_likes' => '2674262',
+                'mean_likes' => '1959.1663',
+                'total_comments' => '39755',
+                'mean_comments' => '29.12454212',
+                'engagement' => '0.053014707',
+            ],
+
+            'topic2' => [
+                "topic_label" => 'topic2',
+                'topic_name' => 'Nature',
+                'total_likes' => '2664356',
+                'mean_likes' => '1164.491259',
+                'total_comments' => '54599',
+                'mean_comments' => '23.8631993',
+                'engagement' => '0.064329351',
+            ],
+
+            'topic20' => [
+                "topic_label" => 'topic20',
+                'topic_name' => 'Food',
+                'total_likes' => '1815696',
+                'mean_likes' => '1040.513467',
+                'total_comments' => '35927',
+                'mean_comments' => '20.58853868',
+                'engagement' => '0.060917027',
+            ],
+
+            'topic6' => [
+                "topic_label" => 'topic6',
+                'topic_name' => 'Gym/Fitness',
+                'total_likes' => '1775357',
+                'mean_likes' => '1703.797505',
+                'total_comments' => '29013',
+                'mean_comments' => '27.84357006',
+                'engagement' => '0.052747313',
+            ],
+
+            'topic26' => [
+                "topic_label" => 'topic26',
+                'topic_name' => 'Beverages',
+                'total_likes' => '1520100',
+                'mean_likes' => '1111.184211',
+                'total_comments' => '31375',
+                'mean_comments' => '22.93494152',
+                'engagement' => '0.048797108',
+            ],
+
+            'topic21' => [
+                "topic_label" => 'topic21',
+                'topic_name' => 'Competition',
+                'total_likes' => '1498451',
+                'mean_likes' => '1313.278703',
+                'total_comments' => '27826',
+                'mean_comments' => '24.38737949',
+                'engagement' => '0.055396845',
+            ],
+
+            'topic13' => [
+                "topic_label" => 'topic13',
+                'topic_name' => 'Footwear',
+                'total_likes' => '1218446',
+                'mean_likes' => '1353.828889',
+                'total_comments' => '24502',
+                'mean_comments' => '27.22444444',
+                'engagement' => '0.052056387',
+            ],
+
+            'topic9' => [
+                "topic_label" => 'topic9',
+                'topic_name' => 'Football',
+                'total_likes' => '1133102',
+                'mean_likes' => '1696.260479',
+                'total_comments' => '15076',
+                'mean_comments' => '22.56886228',
+                'engagement' => '0.053987014',
+            ],
+
+            'topic27' => [
+                "topic_label" => 'topic27',
+                'topic_name' => 'Crowd',
+                'total_likes' => '1114483',
+                'mean_likes' => '1530.883242',
+                'total_comments' => '14430',
+                'mean_comments' => '19.82142857',
+                'engagement' => '0.054706509',
+            ],
+
+            'topic28' => [
+                "topic_label" => 'topic28',
+                'topic_name' => 'Dessert',
+                'total_likes' => '1081874',
+                'mean_likes' => '1053.431353',
+                'total_comments' => '22611',
+                'mean_comments' => '22.01655307',
+                'engagement' => '0.050578015',
+            ],
+
+            'topic29' => [
+                "topic_label" => 'topic29',
+                'topic_name' => 'Fashion 2',
+                'total_likes' => '1069761',
+                'mean_likes' => '1152.759698',
+                'total_comments' => '26958',
+                'mean_comments' => '29.04956897',
+                'engagement' => '0.065810785',
+            ],
+
+            'topic14' => [
+                "topic_label" => 'topic14',
+                'topic_name' => 'Food 2',
+                'total_likes' => '847457',
+                'mean_likes' => '1104.898305',
+                'total_comments' => '14966',
+                'mean_comments' => '19.51238592',
+                'engagement' => '0.062923398',
+            ],
+
+            'topic0' => [
+                "topic_label" => 'topic0',
+                'topic_name' => 'Business Shopping',
+                'total_likes' => '670895',
+                'mean_likes' => '1323.2643',
+                'total_comments' => '12482',
+                'mean_comments' => '24.61932939',
+                'engagement' => '0.046223771',
+            ],
+
+            'topic15' => [
+                "topic_label" => 'topic15',
+                'topic_name' => 'Biking',
+                'total_likes' => '637428',
+                'mean_likes' => '1158.96',
+                'total_comments' => '22076',
+                'mean_comments' => '40.13818182',
+                'engagement' => '0.053839738',
+            ],
+
+            'topic23' => [
+                "topic_label" => 'topic23',
+                'topic_name' => 'Drinks',
+                'total_likes' => '504830',
+                'mean_likes' => '852.7533784',
+                'total_comments' => '9812',
+                'mean_comments' => '16.57432432',
+                'engagement' => '0.054071146',
+            ],
+
+            'topic5' => [
+                "topic_label" => 'topic5',
+                'topic_name' => 'Clocks',
+                'total_likes' => '302578',
+                'mean_likes' => '1039.786942',
+                'total_comments' => '6685',
+                'mean_comments' => '22.97250859',
+                'engagement' => '0.038414281',
+            ],
+        ];
+
+        return $dictionary;
+    }
+}
