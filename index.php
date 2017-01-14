0ee4e756bf95868022d600dcd6d49b8f7e80a18e
Bolaji <bolaji.smith@webgears.de>
2017-01-14 22:50:07 +0100

    3 column user dashboard

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ require 'vendor/autoload.php';
             <?php echo \Reachbird\Services\views::generateInfluencerSelect(); ?>
         </div>
         <div id="user_dashboard" style="width: 100%">
-            <iframe src="user.php" id="dashboard_iframe" scrolling="no" frameBorder="0" style="width: 100%; height: 300px;" onload="this.style.height=this.contentDocument.body.scrollHeight +'px';"></iframe>
+            <iframe src="user.php" id="dashboard_iframe" scrolling="no" frameBorder="0" style="width: 100%;" onload="this.style.height=this.contentDocument.body.scrollHeight +'px';"></iframe>
         </div>
         <div style="width: 100%;"><hr /></div>
 
