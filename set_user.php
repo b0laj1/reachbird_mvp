ba6e368bc36fe8aa4a7be6d39ab45f624cc341a1
Bolaji <bolaji.smith@webgears.de>
2017-01-14 11:21:26 +0100

    add iframe for userdashboarding

--- set_user.php
+++ set_user.php
@@ -2,7 +2,10 @@
 if(! $_SESSION) {
     session_start();
 }
-
-$_SESSION['user_id'] = $_POST['user_id'];
+if($_POST['user_id'] == 'Select Influencer Account'){
+    unset($_SESSION['user_id']);
+} else{
+    $_SESSION['user_id'] = $_POST['user_id'];
+}
 
 echo json_encode(['status'=>1, 'text'=>'done']);
