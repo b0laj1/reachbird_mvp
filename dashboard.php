a7d76dadb0570c6f6994aebc08ff03997d1b387c
Bolaji <bolaji.smith@webgears.de>
2017-01-14 15:16:51 +0100

    add data for likes and comments

--- dashboard.php
+++ dashboard.php
@@ -11,6 +11,7 @@ $chart_data = \Reachbird\Services\data::getGeneralDashboardDataByTopic(10);
 
 
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="emoji-picker-gh-pages/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="de/css/style.css">
     <link rel="stylesheet" type="text/css" href="css/overlay.css">
@@ -32,17 +33,17 @@ $chart_data = \Reachbird\Services\data::getGeneralDashboardDataByTopic(10);
 
     <div class="col-md-6 col-sm-6 col-xs-12">
         <div class="row tile_count">
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
                 <div class="count">2500</div>
                 <span class="count_bottom"><i class="green">4% </i> From last Week</span>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
                 <div class="count">123.50</div>
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
                 <div class="count green">2,500</div>
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
@@ -50,17 +51,17 @@ $chart_data = \Reachbird\Services\data::getGeneralDashboardDataByTopic(10);
         </div>
         <div class="clearfix"></div>
         <div class="row tile_count">
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
                 <div class="count">4,567</div>
                 <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
                 <div class="count">2,315</div>
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
                 <div class="count">7,325</div>
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
