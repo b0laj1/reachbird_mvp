421f8a9d9a18dd7dac869b5ed7f963b8dde050bf
Bolaji <bolaji.smith@webgears.de>
2017-01-14 22:38:44 +0100

    3 column user dashboard

--- user_dashboard.php
+++ user_dashboard.php
@@ -53,7 +53,8 @@ $user_topics = \Reachbird\Services\views::getUserTopics($user);
     </div>
 
     <div class="col-md-4 col-xs-12">
-        <div class="row tile_count">
+        <div id="cloud"></div>
+        <!--<div class="row tile_count">
             <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
                 <div class="count">2500</div>
@@ -70,10 +71,10 @@ $user_topics = \Reachbird\Services\views::getUserTopics($user);
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
             </div>
         </div>
-        <div class="clearfix"></div>
-        <div class="row tile_count">
-            <div id="cloud"></div>
-        </div>
+        <div class="clearfix"></div>-->
+        <!--<div class="row tile_count">
+
+        </div>-->
     </div>
 </div>
 <div class="clearfix"></div>
