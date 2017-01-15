3082af59bb0edc9c6326f14af28f9c98d7cd12d2
Bolaji <bolaji.smith@webgears.de>
2017-01-15 15:42:23 +0100

    order last posts

--- user_dashboard.php
+++ user_dashboard.php
@@ -70,7 +70,7 @@ function dateFromDate($date) {
     </div>
 
     <div class="col-md-4 col-xs-12">
-        <div class="row tile_count">
+        <!--<div class="row tile_count">
             <div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
                 <div class="count">2500</div>
@@ -104,7 +104,7 @@ function dateFromDate($date) {
                 <div class="count">7,325</div>
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
             </div>
-        </div>
+        </div>-->
     </div>
 
     <div class="col-md-4 col-xs-12">
