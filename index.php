41f7bf2f14244996f6a2e9698fce0d6289b16e7b
Bolaji <bolaji.smith@webgears.de>
2017-01-17 20:02:00 +0100

    engagement in percentage and 2dp

--- index.php
+++ index.php
@@ -133,9 +133,9 @@ require 'vendor/autoload.php';
                         var engagement = res.text.x_eng[0] * 100;
                         var comments = res.text.x_comm[0];
                         var likes = res.text.x_likes[0];
-                        $('#eng').val(engagement.toFixed(2) + "%");
-                        $('#comm').val(comments.toFixed(2));
-                        $('#like').val(likes.toFixed(2));
+                        $('#eng').val(Math.round((engagement * 100) / 100) + "%");
+                        $('#comm').val(Math.round((comments * 100) / 100));
+                        $('#like').val(Math.round((likes * 100) / 100));
                     }
                 },
                 error: function (err) {
