704635100be2f0ac21286f73946e2cb2f4905f9a
Bolaji <bolaji.smith@webgears.de>
2017-01-23 17:37:31 +0100

    remove base64 encoding from post request

--- index.php
+++ index.php
@@ -131,9 +131,9 @@ require 'vendor/autoload.php';
                     var res = JSON.parse(response);
                     if(res.status ==1) {
                         console.log(res.text);
-                        var engagement = res.text.x_eng[0] * 100;
-                        var comments = res.text.x_comm[0];
-                        var likes = res.text.x_likes[0];
+                        var engagement = res.text.x_eng * 100;
+                        var comments = res.text.x_comm;
+                        var likes = res.text.x_likes;
                         $('#eng').val(roundToTwo(engagement) + "%");
                         $('#comm').val(roundToTwo(comments));
                         $('#like').val(roundToTwo(likes));
